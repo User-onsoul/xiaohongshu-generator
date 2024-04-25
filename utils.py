aceec99440cafe92d813234813ca1211fb8227a2
leah <53783983+User-onsoul@users.noreply.github.com>
2024-04-25 17:04:51 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,25 @@
+from prompt_template import system_template_text, user_template_text
+from langchain_openai import ChatOpenAI
+from langchain.output_parsers import PydanticOutputParser
+from langchain.prompts import ChatPromptTemplate
+from xiaohongshu_model import Xiaohongshu
+
+import os
+
+
+def generate_xiaohongshu(theme, openai_api_key):
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", system_template_text),
+        ("user", user_template_text)
+    ])
+    model = ChatOpenAI(model="gpt-3.5-turbo", api_key=openai_api_key, base_url="https://api.aigc369.com/v1")
+    output_parser = PydanticOutputParser(pydantic_object=Xiaohongshu)
+    chain = prompt | model | output_parser
+    result = chain.invoke({
+        "parser_instructions": output_parser.get_format_instructions(),
+        "theme": theme
+    })
+    return result
+
+
+# print(generate_xiaohongshu("大模型", os.getenv("OPENAI_API_KEY")))
